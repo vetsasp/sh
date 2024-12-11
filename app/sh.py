a88e0a51480e3cf12db90a3fed6019800bce470a
Philip Vetsas <vetsasp@gmail.com>
2024-12-11 16:42:04 -0500

    Added REPL functionality

--- app/sh.py
+++ app/sh.py
@@ -4,15 +4,17 @@ import sys
 
 class Shell:
     def __init__(self):
-        pass 
+        self.cmds = {}
 
     def run(self):
+        while True:
+            self.runCommand()
+
+    def runCommand(self):
         # prompt
         sys.stdout.write("$ ")
 
         cmd = input()
 
-        cmds = {}
-
-        if cmd not in cmds:
+        if cmd not in self.cmds:
             sys.stdout.write(f"{cmd}: command not found\n")
