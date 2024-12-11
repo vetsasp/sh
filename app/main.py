cd696172358b37eab7155eb377b265958a66cd8c
Philip Vetsas <vetsasp@gmail.com>
2024-12-11 16:35:56 -0500

    Created basic class structure, implemented prompting

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
-import sys
+from app.sh import Shell
 
-
+'''
 def main():
     # Uncomment this block to pass the first stage
     # sys.stdout.write("$ ")
 
     # Wait for user input
     input()
+'''
 
 
 if __name__ == "__main__":
-    main()
+    sh = Shell()
+    sh.run()
