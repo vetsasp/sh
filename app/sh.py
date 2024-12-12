75ea81bddc9c9e117fef1c0f573beb5fe1716662
Philip Vetsas <vetsasp@gmail.com>
2024-12-12 14:44:51 -0500

    codecrafters submit [skip ci]

--- app/sh.py
+++ app/sh.py
@@ -46,7 +46,7 @@ class Shell:
         # parse command and args
         # maintain quotes 
         parts = re.findall(
-            r'(?:\"[^\"]*\"|\'[^\']*\'|\S)+',
+            r'(?:"[^"\\]*(?:\\.[^"\\]*)*"|\'[^\'\\]*(?:\\.[^\'\\]*)*\'|\\(?:[^\'"]|\\.)*|[^\s"\']+)',
               cmd)
         cmd = parts[0] if parts else ''
         args = parts[1:] if len(parts) > 1 else []
